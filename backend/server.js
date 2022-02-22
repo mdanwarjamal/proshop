6162dcb9120efa48e79dc39c8bfd2420da7085fc
Md Anwar Jamal <cse.mdanwarjamal@gmail.com>
2022-02-22 09:37:22 +0530

    Nodemon and Concurrently Setup

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const products = require('./data/products')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Backend is running')
+    res.send('Backend is running...')
 })
 
 app.get('/api/products', (req, res) => {
