203eb805e9a59a8d99c7fb6da9f589f074a3cfba
Md Anwar Jamal <cse.mdanwarjamal@gmail.com>
2022-02-23 21:59:03 +0530

    Using Colors module

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose'
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`Database Connected successfully: ${conn.connection.host}`)
+        console.log(`Database Connected successfully: ${conn.connection.host}`.grey.underline)
     } catch (error) {
-        console.error(`Error: ${error}`)
+        console.error(`Error: ${error}`.red.bold)
         process.exit(1)
     }
 }
